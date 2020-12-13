ca5e14d878a03f91d4b480307f69f72f865bc6f7
jaimegag <jaimegag@gmail.com>
2020-12-12 22:26:02 -0500

    Adding spring cloud, sleuth, for wavefront tests

--- src/test/java/org/springframework/samples/petclinic/PetclinicIntegrationTests.java
+++ src/test/java/org/springframework/samples/petclinic/PetclinicIntegrationTests.java
@@ -19,9 +19,11 @@ package org.springframework.samples.petclinic;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.samples.petclinic.vet.VetRepository;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class PetclinicIntegrationTests {
 
 	@Autowired
